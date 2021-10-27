caa8ccc4cdff70f2699c492976d0877ff472a0d0
Wagner Rose <wagner.rose@gmail.com>
2021-10-27 11:15:58 -0300

    Criado formulario de inclusão de livros

--- app/routes/form_add_book.js
+++ app/routes/form_add_book.js
@@ -2,4 +2,8 @@ module.exports = function(app){
   app.get('/form_add_book', function(req,res){
     res.render('admin/form_add_book');
   });
+  app.post('/books/save', function(req,res){
+    var books = req.body;
+    res.send(books);
+  });
 };

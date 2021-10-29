7abb94bafd85202125c0cb4d67046c5ab157c1e1
Wagner Rose <wagner.rose@gmail.com>
2021-10-29 13:55:07 -0300

    {instalacao do express-validator}

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
+const {check, validationResult} = require('express-validator');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -8,9 +9,6 @@ app.set('views', './app/views');
 
 // ## funciona como midleware
 app.use(bodyParser.urlencoded({extended: true}));
-// app.use(express.json());
-// app.use(express.json({ type: 'application/vnd.api+json' }));
-
 
 consign()
   .include('app/routes')
